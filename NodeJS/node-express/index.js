643f453800a4afd32231c49835bdf24b88c4d9e4
Marcos Medina <marcmedi1@hotmail.com>
2019-07-06 19:23:12 -0300

    REST API implementation with ExpressJS

--- NodeJS/node-express/index.js
+++ NodeJS/node-express/index.js
@@ -1,3 +1,4 @@
+const bodyParser = require("body-parser")
 const express = require("express");
 // const fs = require("fs");
 const morgan = require("morgan");
@@ -8,6 +9,44 @@ const port = 4898;
 
 const app = express();
 app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+app.all("/payments", (req, res, next)=>{
+	res.statusCode=200;
+	res.setHeader("Content-type", "text/plain");
+	next();
+});
+
+app.get("/payments", (req, res, next)=>{
+	res.end("Must to bring all payment registrarion to you!");
+	
+});
+app.post("/payments", (req, res, next)=>{
+	res.end(`Adding a new registration log. Name: "${req.body.name}", Description: "${req.body.description}".`);	
+});
+app.put("/payments", (req, res, next)=>{
+	res.statusCode = 403;
+	res.end("PUT opartion is forbidden on /payments");
+});
+app.delete("/payments", (req, res, next)=>{
+	res.end("Deleting the whole registrations intances.");	
+});
+
+app.get("/payments/:registrationName", (req, res, next)=>{
+	res.end(`Must to bring the payment registrarion "${req.params.registrationName}" to you!`);
+	
+});
+app.post("/payments/:registrationName", (req, res, next)=>{
+	res.statusCode = 403;
+	res.end("POST opartion is forbidden on an already existing resource.");
+});
+app.put("/payments/:registrationName", (req, res, next)=>{
+	res.end(`Updating the registration log. Name: "${req.params.registrationName}" => "${req.body.name}". New description: "${req.body.description}".`);	
+});
+app.delete("/payments/:registrationName", (req, res, next)=>{
+	res.end(`Deleting the registration "${req.params.registrationName}".`);	
+});
+
 app.use(express.static(__dirname+"/public"));
 app.use((req, res, next)=>{
 	res.statusCode=200;
