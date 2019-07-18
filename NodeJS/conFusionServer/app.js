9630a019e8adb815d7b9035d3eb4fabc23781341
Marcos Medina <marcmedi1@hotmail.com>
2019-07-18 19:43:19 -0300

    REST API and Mongoose Exercise 1

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -9,6 +9,19 @@ var usersRouter = require('./routes/users');
 const dishesRouter = require("./routes/dishRouter");
 const leadersRouter = require("./routes/leaderRouter");
 const promotionsRouter = require("./routes/promoRouter");
+const mongoose = require("mongoose");
+const mongooseCurrency = require("mongoose-currency");
+const Dishes = require("./models/dishes");
+const url = "mongodb://localhost:27017/conFusion";
+const connect = mongoose.connect(url);
+
+connect
+.then((db)=>{
+	console.log("Succefully connected to the server!");
+})
+.catch((err)=>{
+	console.error(err);
+});
 
 var app = express();
 
@@ -30,18 +43,18 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
--- NodeJS/conFusionServer/routes/dishRouter.js
+++ NodeJS/conFusionServer/routes/dishRouter.js
@@ -3,45 +3,103 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const dishesRouter = express.Router();
 dishesRouter.use(bodyParser.json());
+const mongoose = require("mongoose");
+const Dishes = require("../models/dishes");
 
 // global endpoint
 dishesRouter.route("/")
-.all((req, res, next)=>{
-	res.statusCode=200;
-	res.setHeader("Content-type", "text/plain");
-	next();
-})
 .get((req, res, next)=>{
-	res.end("Must to bring all dishes list to you!");
-	
+	console.log("\n\nGetting the whole list of dishes:\n")
+	Dishes.find({})
+		.then((data)=>{
+			res.statusCode = 200;
+			res.setHeader("Content-type", "application/json");
+			res.json(data);
+		}, (err) => next(err))
+		.catch((err)=>{
+			console.error("Error in get >>> ", err);
+		});
 })
 .post((req, res, next)=>{
-	res.end(`Adding a new dishe to the list. Name: "${req.body.name}", Description: "${req.body.description}".`);	
+	Dishes.create(req.body)
+	.then((dish)=>{
+		res.statusCode = 200;
+		res.setHeader("Content-type", "application/json");
+		res.json(dish);
+		console.log("\nNew dish added:\n", dish);
+	}, (err) => next(err))
+	.catch((err)=>{
+		Dishes.remove({});
+		console.error("Error in post >>> ", err);
+	});
 })
 .put((req, res, next)=>{
 	res.statusCode = 403;
 	res.end("PUT opartion is forbidden on /dishes");
 })
 .delete((req, res, next)=>{
-	res.end("Deleting the whole dishes list.");	
+	Dishes.remove({})
+	.then((resp)=>{
+		console.log("\n\nDeleting all instances.\n");
+		res.statusCode = 200;
+		res.json(resp);
+		console.log("\n\nAll instances has been deleted.\n");
+	}, (err) => next(err))
+	.catch((err)=>{
+		console.error("Error in delete >>> ", err);
+	});
 });
 
 // endpoint with id
 dishesRouter.route("/:dishId")
 .get((req, res, next)=>{
-	res.end(`The get action has returned the dish with id: "${req.params.dishId}"`);
+	console.log("\n\nGetting the dishe: with id: "+req.params.dishId);
+	Dishes.findById(req.params.dishId)
+		.then((data)=>{
+			res.statusCode = 200;
+			res.setHeader("Content-type", "application/json");
+			res.json(data);
+		}, (err) => next(err))
+		.catch((err)=>{
+			console.error("Error in get /id >>> ", err);
+		});
 })
 .post((req, res, next)=>{
 	res.statusCode = 403;
 	res.end("POST opartion is forbidden on an already existing resource.");
 })
 .put((req, res, next)=>{
-	res.write("<i> params to include in the body of request: 'name', 'price' </i>");
-	res.write("\n");
-	res.end(`Updating the dish with id: "${req.params.dishId}". New Name: "${req.body.name}". New description: "${req.body.price}".`);	
+	console.log("\n\nGetting the dish with id: "+req.params.dishId);
+	
+	Dishes.findByIdAndUpdate(
+		req.params.dishId, 
+		{
+			$set: req.body
+		},
+		{
+			new: true
+		}
+	)
+	.then((data)=>{
+		res.statusCode = 200;
+		res.setHeader("Content-type", "application/json");
+		res.json(data);
+	}, (err) => next(err))
+	.catch((err)=>{
+		console.error("Error in put /id >>> ", err);
+	});
 })
 .delete((req, res, next)=>{
-	res.end(`Deleting the dish with id: "${req.params.dishId}".`);	
+	console.log("\n\nDeleting the dish with id: "+req.params.dishId);
+	Dishes.findByIdAndRemove(req.params.dishId)
+	.then((data)=>{
+		res.statusCode = 200;
+		res.setHeader("Content-type", "application/json");
+		res.json(data);
+	}, (err) => next(err))
+	.catch((err)=>{
+		console.error("Error in delete /id >>> ", err);
+	});	
 });
 
 module.exports = dishesRouter;
