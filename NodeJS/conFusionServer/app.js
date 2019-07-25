0c650499e10d9c958b23847d52d3ad150d4436c5
Marcos Á. Medina <33690842+MarkMed@users.noreply.github.com>
2019-07-25 15:24:06 -0300

    setting of cookie, comments added to explain each step. MUST TO BE COMPLETED!

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -32,30 +32,55 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser("0419-0809-2000-1998"));
 
 function getAuthorization(req, res, next){
-	console.log(req.headers);
-	const authorizationHeader = req.headers.authorization;
-	// Checks if the authorizationHeader has value or not. If null, it ends the function and retunr an error
-	if(!authorizationHeader){
-		const err = new Error("You are not authenticated. Please, log in your account, or create one.");
-		res.setHeader("WWW-Authenticate", "Basic");
-		err.status = 401;
-		return next(err);
-	}
-	const authorization = new Buffer(authorizationHeader.split(" ")[1], "base64").toString().split(":");
-	const username = authorization[0];
-	const password = authorization[1];
+	const reqHeaders = req.headers;
+	const signedCookie = req.signedCookies;
+	console.log(signedCookie);
 
-	if(username === "markmed" && password === "accessMkMServer4"){
-		return next();
+	if(!signedCookies.user){
+		// Check if exist the user property setted in the signedCookie	or the signedCookie itself is already installed, if null, it...
+
+		const authorizationHeader = req.headers.authorization;
+		// ...challenge the user to authenticate and save authenticate info from the header when user submit...
+		if(!authorizationHeader){
+		// ...it checks if the authorizationHeader has value or not. If null...
+			const err = new Error("You are not authenticated. Please, log in your account, or create one.");
+			res.setHeader("WWW-Authenticate", "Basic");
+			err.status = 401;
+			// ...it ends the function and retunr an error...
+			return next(err);
+		}
+		//...else if authorizationHeader has value, it analize the value wich coms within authorizationHeader...
+		const authorization = new Buffer(authorizationHeader.split(" ")[1], "base64").toString().split(":");
+		const username = authorization[0];
+		const password = authorization[1];
+		//...saves in variables for better understanding...
+
+		if(username === "markmed" && password === "accessMkMServer4"){
+		//...checks if values are valid. If are valid...
+			res.cookie("user", "markmed", {signed: true});
+			//...set the cookie with info and sign it...
+			return next();
+		}
+		else{
+			//...else... SEGUIR CON EXPLICACIÓN
+			const err = new Error("Wrong username or password");
+			res.setHeader("WWW-Authenticate", "Basic");
+			err.status = 401;
+			return next(err);
+		}
 	}
-	else{
-		const err = new Error("Wrong username or password");
-		res.setHeader("WWW-Authenticate", "Basic");
-		err.status = 401;
-		return next(err);
+	else{ // ...else 
+		if(signedCookies.user === "markmed"){
+			return next();
+		}
+		else{
+			const err = new Error("You are not authenticated. Please, log in your account, or create one.");
+			err.status = 401;
+			return next(err);
+		}
 	}
 }
 app.use(getAuthorization);
