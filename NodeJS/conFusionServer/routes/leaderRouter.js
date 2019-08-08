e80cc7ea20b4110a575c7e267ff1289846531d14
Marcos Medina <marcmedi1@hotmail.com>
2019-08-08 18:44:47 -0300

    rest of routes fixed with new admin verification

--- NodeJS/conFusionServer/routes/leaderRouter.js
+++ NodeJS/conFusionServer/routes/leaderRouter.js
@@ -28,46 +28,34 @@ leadersRouter.route("/")
 			console.error("Error in get >>> ", err);
 		});
 })
-.post(authenticate.verifyUser, (req, res, next)=>{
-	if(authenticate.verifyAdmin(req)){
-		Leaders.create(req.body)
-		.then((data)=>{
-			res.statusCode = 200;
-			res.setHeader("Content-type", "application/json");
-			res.json(data);
-			console.log("\nNew leader added:\n", data);
-		}, (err) => next(err))
-		.catch((err)=>{
-			Leaders.remove({});
-			console.error("Error in post >>> ", err);
-		});
-	}
-	else {
-		res.statusCode = 403;
-		res.end("You account does not have the privileges to do this operation.");
-	}
+.post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next)=>{
+	Leaders.create(req.body)
+	.then((data)=>{
+		res.statusCode = 200;
+		res.setHeader("Content-type", "application/json");
+		res.json(data);
+		console.log("\nNew leader added:\n", data);
+	}, (err) => next(err))
+	.catch((err)=>{
+		Leaders.remove({});
+		console.error("Error in post >>> ", err);
+	});
 })
 .put(authenticate.verifyUser, (req, res, next)=>{
 	res.statusCode = 403;
 	res.end("PUT opartion is forbidden on /leaders");
 })
 .delete(authenticate.verifyUser, (req, res, next)=>{
-	if(authenticate.verifyAdmin(req)){
-		Leaders.remove({})
-		.then((resp)=>{
-			console.log("\n\nDeleting all instances.\n");
-			res.statusCode = 200;
-			res.json(resp);
-			console.log("\n\nAll instances has been deleted.\n");
-		}, (err) => next(err))
-		.catch((err)=>{
-			console.error("Error in delete >>> ", err);
-		});
-	}
-	else {
-		res.statusCode = 403;
-		res.end("You account does not have the privileges to do this operation.");
-	}
+	Leaders.remove({})
+	.then((resp)=>{
+		console.log("\n\nDeleting all instances.\n");
+		res.statusCode = 200;
+		res.json(resp);
+		console.log("\n\nAll instances has been deleted.\n");
+	}, (err) => next(err))
+	.catch((err)=>{
+		console.error("Error in delete >>> ", err);
+	});
 });
 
 // endpoint with id
@@ -96,71 +84,59 @@ leadersRouter.route("/:leaderId")
 	res.statusCode = 403;
 	res.end("POST opartion is forbidden on an already existing resource.");
 })
-.put(authenticate.verifyUser, (req, res, next)=>{
-	if(authenticate.verifyAdmin(req)){
-		const leaderId = req.params.leaderId;
-		console.log("\n\nUpdating the leader with id: "+leaderId);
-		Leaders.findById(leaderId)
-		.then((data)=>{
-			if(data){
-				Leaders.findByIdAndUpdate(
-					leaderId, 
-					{
-						$set: req.body
-					},
-					{
-						new: true
-					}
-				)
-				.then((data)=>{
-					res.statusCode = 200;
-					res.setHeader("Content-type", "application/json");
-					res.json(data);
-				}, (err) => next(err));
-			}
-			else{
-				err = new Error(`Leader #${leaderId} not found.`);
-				err.status = 404;
-				return next(err);
-			}
-		}, (err) => next(err))
-		.catch((err)=>{
-			console.error("Error in put /id >>> ", err);
-		});
-	}
-	else {
-		res.statusCode = 403;
-		res.end("You account does not have the privileges to do this operation.");
-	}
+.put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next)=>{
+	const leaderId = req.params.leaderId;
+	console.log("\n\nUpdating the leader with id: "+leaderId);
+	Leaders.findById(leaderId)
+	.then((data)=>{
+		if(data){
+			Leaders.findByIdAndUpdate(
+				leaderId, 
+				{
+					$set: req.body
+				},
+				{
+					new: true
+				}
+			)
+			.then((data)=>{
+				res.statusCode = 200;
+				res.setHeader("Content-type", "application/json");
+				res.json(data);
+			}, (err) => next(err));
+		}
+		else{
+			err = new Error(`Leader #${leaderId} not found.`);
+			err.status = 404;
+			return next(err);
+		}
+	}, (err) => next(err))
+	.catch((err)=>{
+		console.error("Error in put /id >>> ", err);
+	});
 })
-.delete(authenticate.verifyUser, (req, res, next)=>{
-	if(authenticate.verifyAdmin(req)){
-		const leaderId = req.params.leaderId;
-		console.log("\n\nDeleting the leader with id: "+leaderId);
-		Leaders.findById(leaderId)
-		.then((data)=>{
-			if(data){
-				Leaders.findByIdAndRemove(leaderId)
-				.then((data)=>{
-					res.statusCode = 200;
-					res.setHeader("Content-type", "application/json");
-					res.json(data);
-				}, (err) => next(err));
-			}
-			else{
-				err = new Error(`Leader #${leaderId} not found.`);
-				err.status = 404;
-				return next(err);
-			}
-		}, (err) => next(err))
-		.catch((err)=>{
-			console.error("Error in get /id >>> ", err);
-		});
-	}
-	else {
-		res.statusCode = 403;
-		res.end("You account does not have the privileges to do this operation.");
-	}
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next)=>{
+	const leaderId = req.params.leaderId;
+	console.log("\n\nDeleting the leader with id: "+leaderId);
+	Leaders.findById(leaderId)
+	.then((data)=>{
+		if(data){
+			Leaders.findByIdAndRemove(leaderId)
+			.then((data)=>{
+				res.statusCode = 200;
+				res.setHeader("Content-type", "application/json");
+				res.json(data);
+			}, (err) => next(err));
+		}
+		else{
+			err = new Error(`Leader #${leaderId} not found.`);
+			err.status = 404;
+			return next(err);
+		}
+	}, (err) => next(err))
+	.catch((err)=>{
+		console.error("Error in get /id >>> ", err);
+	});
 });
 
 module.exports = leadersRouter;
--- NodeJS/conFusionServer/routes/promoRouter.js
+++ NodeJS/conFusionServer/routes/promoRouter.js
@@ -28,46 +28,34 @@ promotionsRouter.route("/")
 			console.error("Error in get >>> ", err);
 		});
 })
-.post(authenticate.verifyUser, (req, res, next)=>{	
-	if(authenticate.verifyAdmin(req)){
-		Promotions.create(req.body)
-		.then((data)=>{
-			res.statusCode = 200;
-			res.setHeader("Content-type", "application/json");
-			res.json(data);
-			console.log("\nNew promotion added:\n", data);
-		}, (err) => next(err))
-		.catch((err)=>{
-			Promotions.remove({});
-			console.error("Error in post >>> ", err);
-		});
-	}
-	else {
-		res.statusCode = 403;
-		res.end("You account does not have the privileges to do this operation.");
-	}
+.post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next)=>{
+	Promotions.create(req.body)
+	.then((data)=>{
+		res.statusCode = 200;
+		res.setHeader("Content-type", "application/json");
+		res.json(data);
+		console.log("\nNew promotion added:\n", data);
+	}, (err) => next(err))
+	.catch((err)=>{
+		Promotions.remove({});
+		console.error("Error in post >>> ", err);
+	});
 })
 .put(authenticate.verifyUser, (req, res, next)=>{
 	res.statusCode = 403;
 	res.end("PUT opartion is forbidden on /promotions");
 })
-.delete(authenticate.verifyUser, (req, res, next)=>{
-	if(authenticate.verifyAdmin(req)){
-		Promotions.remove({})
-		.then((resp)=>{
-			console.log("\n\nDeleting all instances.\n");
-			res.statusCode = 200;
-			res.json(resp);
-			console.log("\n\nAll instances has been deleted.\n");
-		}, (err) => next(err))
-		.catch((err)=>{
-			console.error("Error in delete >>> ", err);
-		});
-	}
-	else {
-		res.statusCode = 403;
-		res.end("You account does not have the privileges to do this operation.");
-	}
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next)=>{
+	Promotions.remove({})
+	.then((resp)=>{
+		console.log("\n\nDeleting all instances.\n");
+		res.statusCode = 200;
+		res.json(resp);
+		console.log("\n\nAll instances has been deleted.\n");
+	}, (err) => next(err))
+	.catch((err)=>{
+		console.error("Error in delete >>> ", err);
+	});
 });
 
 // endpoint with id
@@ -96,71 +84,59 @@ promotionsRouter.route("/:promotionId")
 	res.statusCode = 403;
 	res.end("POST opartion is forbidden on an already existing resource.");
 })
-.put(authenticate.verifyUser, (req, res, next)=>{
-	if(authenticate.verifyAdmin(req)){
-		const promotionId = req.params.promotionId;
-		console.log("\n\nUpdating the promotion with id: "+promotionId);
-		Promotions.findById(promotionId)
-		.then((data)=>{
-			if(data){
-				Promotions.findByIdAndUpdate(
-					promotionId, 
-					{
-						$set: req.body
-					},
-					{
-						new: true
-					}
-				)
-				.then((data)=>{
-					res.statusCode = 200;
-					res.setHeader("Content-type", "application/json");
-					res.json(data);
-				}, (err) => next(err));
-			}
-			else{
-				err = new Error(`Promotion #${promotionId} not found.`);
-				err.status = 404;
-				return next(err);
-			}
-		}, (err) => next(err))
-		.catch((err)=>{
-			console.error("Error in put /id >>> ", err);
-		});
-	}
-	else {
-		res.statusCode = 403;
-		res.end("You account does not have the privileges to do this operation.");
-	}
+.put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next)=>{
+	const promotionId = req.params.promotionId;
+	console.log("\n\nUpdating the promotion with id: "+promotionId);
+	Promotions.findById(promotionId)
+	.then((data)=>{
+		if(data){
+			Promotions.findByIdAndUpdate(
+				promotionId, 
+				{
+					$set: req.body
+				},
+				{
+					new: true
+				}
+			)
+			.then((data)=>{
+				res.statusCode = 200;
+				res.setHeader("Content-type", "application/json");
+				res.json(data);
+			}, (err) => next(err));
+		}
+		else{
+			err = new Error(`Promotion #${promotionId} not found.`);
+			err.status = 404;
+			return next(err);
+		}
+	}, (err) => next(err))
+	.catch((err)=>{
+		console.error("Error in put /id >>> ", err);
+	});
 })
-.delete(authenticate.verifyUser, (req, res, next)=>{
-	if(authenticate.verifyAdmin(req)){
-		const promotionId = req.params.promotionId;
-		console.log("\n\nDeleting the promotion with id: "+promotionId);
-		Promotions.findById(promotionId)
-		.then((data)=>{
-			if(data){
-				Promotions.findByIdAndRemove(promotionId)
-				.then((data)=>{
-					res.statusCode = 200;
-					res.setHeader("Content-type", "application/json");
-					res.json(data);
-				}, (err) => next(err));
-			}
-			else{
-				err = new Error(`Promotion #${promotionId} not found.`);
-				err.status = 404;
-				return next(err);
-			}
-		}, (err) => next(err))
-		.catch((err)=>{
-			console.error("Error in get /id >>> ", err);
-		});
-	}
-	else {
-		res.statusCode = 403;
-		res.end("You account does not have the privileges to do this operation.");
-	}
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next)=>{
+	const promotionId = req.params.promotionId;
+	console.log("\n\nDeleting the promotion with id: "+promotionId);
+	Promotions.findById(promotionId)
+	.then((data)=>{
+		if(data){
+			Promotions.findByIdAndRemove(promotionId)
+			.then((data)=>{
+				res.statusCode = 200;
+				res.setHeader("Content-type", "application/json");
+				res.json(data);
+			}, (err) => next(err));
+		}
+		else{
+			err = new Error(`Promotion #${promotionId} not found.`);
+			err.status = 404;
+			return next(err);
+		}
+	}, (err) => next(err))
+	.catch((err)=>{
+		console.error("Error in get /id >>> ", err);
+	});
 });
 
 module.exports = promotionsRouter;
